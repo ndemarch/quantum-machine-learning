d44332ccbd15fac198511c39dfaa901394139894
ndemarch <73549112+ndemarch@users.noreply.github.com>
2024-10-29 19:01:51 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 # Execute preprocess.py
 python preprocess.py
 
-# Wait for 5 seconds before proceeding to the next script
+# Wait for 2 seconds before proceeding to the next script
 sleep 2
 
 # Execute stats_analysis.py
 python3 stats_analysis.py
 
-# Wait for 5 seconds before proceeding to the next script
+# Wait for 2 seconds before proceeding to the next script
 sleep 2
 
 # Execute model.py
